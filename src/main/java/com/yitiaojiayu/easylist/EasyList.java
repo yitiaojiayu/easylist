4456b13da1420c85cef855d88138899614bdc74a
yitiaojiayu <1945248690@qq.com>
2025-03-29 15:47:03 +0800

    feat: add, get, size, isEmpty, toString

--- src/main/java/com/yitiaojiayu/easylist/EasyList.java
+++ src/main/java/com/yitiaojiayu/easylist/EasyList.java
@@ -1,7 +1,6 @@
 package com.yitiaojiayu.easylist;
 
 import com.yitiaojiayu.kryo.KryoSimple;
-
 import java.nio.ByteBuffer;
 import java.util.Collection;
 import java.util.Iterator;
@@ -12,19 +11,69 @@ import java.util.ListIterator;
  * @author yitiaojiayu
  * @date 2025/3/27
  */
-@SuppressWarnings("AlibabaConstantFieldShouldBeUpperCase")
+@SuppressWarnings({"AlibabaConstantFieldShouldBeUpperCase", "unused", "java:S106"})
 public class EasyList<E> implements List<E> {
 
-    private final ByteBuffer buffer;
+    private static final int DEFAULT_BUFFER_CAPACITY = 1024;
+    private static final int DEFAULT_LIMIT_SIZE = 10;
+
+    private ByteBuffer buffer;
+    private int limit;
     private int count;
-    private final int[] index;
-    private final int[] size;
+    private int[] index;
+    private int[] size;
     private int memoryUsed;
 
     public EasyList() {
-        buffer = ByteBuffer.allocateDirect(1024);
-        index = new int[10];
-        size = new int[10];
+        buffer = ByteBuffer.allocateDirect(DEFAULT_BUFFER_CAPACITY);
+        limit = DEFAULT_LIMIT_SIZE;
+        this.count = 0;
+        index = new int[limit];
+        size = new int[limit];
+        this.memoryUsed = 0;
+    }
+
+    private void resizeBuffer() {
+        ByteBuffer newBuffer = ByteBuffer.allocateDirect(buffer.capacity() * 2);
+        buffer.position(0);
+        buffer.limit(memoryUsed);
+        newBuffer.put(buffer);
+        this.buffer = newBuffer;
+    }
+
+    private void resizeArrays() {
+        int newLimit = limit * 2;
+        int[] newIndex = new int[newLimit];
+        int[] newSize = new int[newLimit];
+        System.arraycopy(index, 0, newIndex, 0, limit);
+        System.arraycopy(size, 0, newSize, 0, limit);
+        index = newIndex;
+        size = newSize;
+        limit = newLimit;
+    }
+
+    private boolean memorySufficient(E e) {
+        int memorySize = KryoSimple.asByteArray(e).length;
+        int memoryRemaining = buffer.capacity() - memoryUsed;
+        return memoryRemaining >= memorySize;
+    }
+
+    private boolean arraySufficient() {
+        return limit > count;
+    }
+
+    private void detect(E e) {
+        if (!memorySufficient(e)) {
+            resizeBuffer();
+        }
+        if (!arraySufficient()) {
+            resizeArrays();
+        }
+    }
+
+    private E getData(int i) {
+        byte[] data = EasyListNative.get(buffer, index[i], size[i]);
+        return KryoSimple.asObject(data);
     }
 
     @Override
@@ -59,6 +108,7 @@ public class EasyList<E> implements List<E> {
 
     @Override
     public boolean add(E e) {
+        detect(e);
         byte[] data = KryoSimple.asByteArray(e);
         buffer.put(data);
         index[count] = memoryUsed;
@@ -106,10 +156,9 @@ public class EasyList<E> implements List<E> {
     @Override
     public E get(int i) {
         if (i < 0 || i >= count) {
-            throw new IndexOutOfBoundsException("Index: " + i + ", Size: " + count);
+            throw new IndexOutOfBoundsException("EasyList: get: failed, Because Index: " + i + ", Size: " + count);
         }
-        byte[] data = EasyListNative.get(buffer, index[i], size[i]);
-        return KryoSimple.asObject(data);
+        return getData(i);
     }
 
     @Override
@@ -149,5 +198,21 @@ public class EasyList<E> implements List<E> {
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
         return List.of();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        int i = 0;
+        while (count > i) {
+            sb.append(getData(i));
+            if (count > i + 1) {
+                sb.append(", ");
+            }
+            i ++;
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
